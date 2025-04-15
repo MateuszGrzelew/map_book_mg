7a5c8fd4f09a7a8c761068ed2e9aeaabd6b568f5
MateuszGrzelew <rafal.matek2005@gmail.com>
2025-04-15 18:30:31 +0200

    wip: add new user funcionality

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from random import choice
-
 from utils.model import users
-from utils.controler import get_user_info
+from utils.controler import get_user_info, add_user
 
 
 def main():
     print('=========menu=========')
     print('0 - zakończ program')
     print('1 - wyświetl co u znajomych')
+    print('2 - dodaj znajomego')
     print('======================')
     while True:
         choice: str = input('wybierz opcię menu: ')
         if choice == '0': break
         if choice == '1': get_user_info(users)
+        if choice == '2': add_user(users)
 
 
 if __name__ == '__main__':
--- utils/model.py
+++ utils/model.py
@@ -1,6 +1,6 @@
-users: list = [
-    {'name': 'Mateusz', 'location': 'Poznań', 'posts': 100},
-    {'name': 'Maciej', 'location': 'Łódż', 'posts': 200},
-    {'name': 'Maciej01', 'location': 'Siedlice', 'posts': 300},
-    {'name': 'Konrad', 'location': 'Grudziądz', 'posts': 400},
+users:list=[
+    {'name':'Mateusz','location':'Poznań','posts':100},
+    {'name':'Maciej','location':'Łódż','posts':200},
+    {'name':'Maciej01','location':'Siedlice','posts':300},
+    {'name':'Konrad','location':'Grudziądz','posts':400},
 ]
